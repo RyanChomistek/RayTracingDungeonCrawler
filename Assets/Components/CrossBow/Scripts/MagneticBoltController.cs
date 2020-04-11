935ca3d4aafc9efac04feddfb16afe063a3ea83d
Ryan Chomistek <35873399+RyanChomistek@users.noreply.github.com>
2020-04-10 22:01:43 -0700

    touched up magnetic bolt controller

--- Assets/Components/CrossBow/Scripts/MagneticBoltController.cs
+++ Assets/Components/CrossBow/Scripts/MagneticBoltController.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class MagneticBoltController : CrossBowBoltController
 {
+    [SerializeField]
+    private float m_AttractionForceInitial = 5;
+
+    [SerializeField]
+    private float m_AttractionForceDuration = 1;
+
     private void OnTriggerEnter(Collider other)
     {
         CrossBowBoltController boltController = other.gameObject.GetComponent<CrossBowBoltController>();
         if (boltController != null && boltController.HasBeenFired)
         {
-            Debug.Log(other.gameObject.name);
-            boltController.OnFire(gameObject, m_Collider, m_RB.velocity.normalized);
-            other.attachedRigidbody.velocity = m_RB.velocity;
+            Vector3 dir = this.transform.position - boltController.transform.position;
+
+            boltController.OnFire(gameObject, m_Collider, dir * m_AttractionForceInitial);
+            other.attachedRigidbody.velocity = dir * m_AttractionForceInitial;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        CrossBowBoltController boltController = other.gameObject.GetComponent<CrossBowBoltController>();
+        if (boltController != null && boltController.HasBeenFired)
+        {
+            Vector3 dir = this.transform.position - boltController.transform.position;
+
+            boltController.OnFire(gameObject, m_Collider, dir * m_AttractionForceDuration);
+            other.attachedRigidbody.velocity = dir * m_AttractionForceDuration;
         }
     }
 
